feec178d55f5c977345e26f171c085cfbaed4446
ironman <2012joe@gmail.com>
2020-07-11 11:43:47 +0800

    feat(index.js): import ApolloClient instance

--- src/index.js
+++ src/index.js
@@ -3,11 +3,35 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+// 1. You’re importing the required dependencies from the installed packages.
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
+/**
+ * 2. Here you create the httpLink that will connect your ApolloClient instance with the GraphQL API, 
+ *    your GraphQL server will be running on http://localhost:4000.
+ */
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+/**
+ * 3. Now you instantiate ApolloClient by passing in the httpLink and a new instance of an InMemoryCache.
+ */
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+/**
+ * 4. Finally you render the root component of your React app. The App is wrapped with the higher-order component 
+ *    ApolloProvider that gets passed the client as a prop.
+ */
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
